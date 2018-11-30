4f74bbf836272c088a0d82331bc5c6ed8978b4ad
Kyle Durigan <kyle.durigan@gmail.com>
2018-11-30 10:26:05 -0700

    app.js - symbols now pull info on selected stock and adds to table. Added Variables for placeholeders

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 
 //SECTION FIREBASE
     // Initialize Firebase
     var config = {
-=======
- // Initialize Firebase
-  var config = {
->>>>>>> 2610cba646263f85daf3557a52fa4747429cc886
     apiKey: "AIzaSyAsBSMWdIEzARseH2Ar2_L3B-z3pxUeSJs",
     authDomain: "trademark-c83fc.firebaseapp.com",
     databaseURL: "https://trademark-c83fc.firebaseio.com",
@@ -16,14 +11,23 @@
     };
     firebase.initializeApp(config);
     var database = firebase.database();
-
-// SECTION: WATCHLIST TABLE
     
 
 //Variables
 var symbol;
+var stockName;
+var lastPrice;
+var percentChange;
+var openPrice;
+var fiftyTwoWkHigh;
+var fiftyTwoWkLow;
+var dividendRate;
+var dividendYeild;
+var exDividendDate;
+
 
 //Functions
+// SECTION: WATCHLIST TABLE
 $("#submitButton").on("click", function(event) {
     event.preventDefault();
     symbolInput = $("#symbolInput").val().trim();
@@ -31,13 +35,27 @@ $("#submitButton").on("click", function(event) {
     var onDemand = new OnDemandClient();
     onDemand.setAPIKey('b07ee9357cf8f37a122e8176286d33c7');
     onDemand.setJsonP(true);
-    /* get a quote for AAPL and GOOG */
     onDemand.getQuote({symbols: symbolInput}, function (err, data) {
             var quotes = data.results;
             for (x in quotes) {
-                console.log("getQuote: " + quotes[x].symbol + " [" + quotes[x].name + "] = " + JSON.stringify(quotes[x]));
-            }
+            symbol = quotes[x].symbol;
+            stockName = quotes[x].name;
+            lastPrice = quotes[x].lastPrice;
+            percentChange = quotes[x].percentChange;
+            openPrice = quotes[x].open;
+            fiftyTwoWkHigh = quotes[x].fiftyTwoWkHigh;
+            fiftyTwoWkLow = quotes[x].fiftyTwoWkLow;
+            dividendRate = quotes[x].dividendRateAnnual;
+            dividendYeild = quotes[x].dividendYieldAnnual;
+            exDividendDate = quotes[x].exDividendDate;
+            console.log(fiftyTwoWkHigh, symbol, stockName, lastPrice, percentChange, openPrice);
+            };
+        addToTable ();
     });
 });
+
+function addToTable () {
+    $("tbody").append("<tr><th scope='row'>" + stockName +"</th> <td>" + lastPrice + "</td> <td>" + percentChange + "</td> <td>" + '52 High' + "</td> <td>" + '52 Low' + "</td></tr>");
+};
 
 //Main Processes
